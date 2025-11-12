4c02201467b2d89e78af136814f840e5dba5c2c9
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2025-11-13 00:32:07 +0100

    add skeleton for fields

--- src/components/form/camera.tsx
+++ src/components/form/camera.tsx
@@ -10,37 +10,38 @@ import {Aperture, Focus, Move3d, ScanEye} from 'lucide-react'
 import {Slider} from '@/components/ui/slider'
 import CoordinateInput from './coordinateInput'
 
+const iconSize = '16'
+
 export type CameraFieldProps = {
 	camera: CameraSpec
 	onChange: (changes: Partial<CameraSpec>) => void
 }
 
-export default function CameraFieldSet({
-	camera,
-	onChange,
-}: Readonly<CameraFieldProps>) {
+export default function CameraFieldSet(props: Readonly<CameraFieldProps>) {
 	return (
 		<FieldSet>
 			<FieldGroup>
-				<FieldOfViewField camera={camera} onChange={onChange} />
-				<ApertureField camera={camera} onChange={onChange} />
-				<FocusDistanceField camera={camera} onChange={onChange} />
-				<SourceField camera={camera} onChange={onChange} />
+				<FieldOfViewField {...props} />
+				<ApertureField {...props} />
+				<FocusDistanceField {...props} />
+				<SourceField {...props} />
 			</FieldGroup>
 		</FieldSet>
 	)
 }
 
 function FieldOfViewField({camera, onChange}: Readonly<CameraFieldProps>) {
+	const id = 'fov'
 	return (
-		<Field>
-			<FieldLabel className='flex items-center gap-2' htmlFor='fov'>
-				<ScanEye size='16' />
-				Field of View
-			</FieldLabel>
+		<Skeleton
+			id={id}
+			icon={<ScanEye size={iconSize} />}
+			label='Field of View'
+			description='The vertical angle visible in the rendered image.'
+		>
 			<div className='flex gap-2 align-center items-center'>
 				<Slider
-					id='fov'
+					id={id}
 					value={[camera.fov]}
 					onValueChange={(v) => onChange({fov: v[0]})}
 					min={1}
@@ -51,81 +52,96 @@ function FieldOfViewField({camera, onChange}: Readonly<CameraFieldProps>) {
 				/>
 				<span className='w-[2em] text-right'>{camera.fov}&deg;</span>
 			</div>
-			<FieldDescription>
-				The vertical angle visible in the rendered image.
-			</FieldDescription>
-		</Field>
+		</Skeleton>
 	)
 }
 
 function ApertureField({camera, onChange}: Readonly<CameraFieldProps>) {
+	const id = 'aperture'
 	return (
-		<Field>
-			<FieldLabel className='flex items-center gap-2' htmlFor='fov'>
-				<Aperture size='16' />
-				Depth of Field
-			</FieldLabel>
+		<Skeleton
+			id={id}
+			icon={<Aperture size={iconSize} />}
+			label='Depth of Field'
+			description='The blurriness of objects that are out of focus.'
+		>
 			<div className='flex gap-2 align-center items-center'>
 				<Slider
-					id='fov'
+					id={id}
 					value={[camera.aperture]}
 					onValueChange={(v) => onChange({aperture: v[0]})}
 					min={0}
 					max={50}
 					step={0.5}
-					aria-label='Field of View'
+					aria-label='Depth of Field'
 					role='slider'
 				/>
 				<span className='w-[2em] text-right'>{camera.aperture}</span>
 			</div>
-			<FieldDescription>
-				The blurriness of objects that are out of focus.
-			</FieldDescription>
-		</Field>
+		</Skeleton>
 	)
 }
 
 function FocusDistanceField({camera, onChange}: Readonly<CameraFieldProps>) {
+	const id = 'focus-distance'
 	return (
-		<Field>
-			<FieldLabel className='flex items-center gap-2' htmlFor='fov'>
-				<Focus size='16' />
-				Focus Distance
-			</FieldLabel>
+		<Skeleton
+			id={id}
+			icon={<Focus size={iconSize} />}
+			label='Focus Distance'
+			description='The distance from the camera to the object in focus.'
+		>
 			<div className='flex gap-2 align-center items-center'>
 				<Slider
-					id='fov'
+					id={id}
 					value={[camera.focusDistance]}
 					onValueChange={(v) => onChange({focusDistance: v[0]})}
 					min={0.5}
 					max={50}
 					step={0.5}
-					aria-label='Field of View'
+					aria-label='Focus Distance'
 					role='slider'
 				/>
 				<span className='w-[2em] text-right'>{camera.focusDistance}</span>
 			</div>
-			<FieldDescription>
-				The distance from the camera to the object in focus.
-			</FieldDescription>
-		</Field>
+		</Skeleton>
 	)
 }
 
 function SourceField({camera, onChange}: Readonly<CameraFieldProps>) {
 	return (
-		<Field className='overflow-visible'>
-			<FieldLabel className='flex items-center gap-2' htmlFor='fov'>
-				<Move3d size='16' />
-				Location
-			</FieldLabel>
+		<Skeleton
+			id='source'
+			icon={<Move3d size={iconSize} />}
+			label='Location'
+			description='The point where the camera is located.'
+		>
 			<CoordinateInput
 				values={camera.source}
 				onChange={(v) => onChange({source: v})}
 			/>
-			<FieldDescription>
-				The point where the camera is located.
-			</FieldDescription>
+		</Skeleton>
+	)
+}
+
+type SkeletonProps = {
+	id?: string
+	icon?: React.ReactNode
+	label: string
+	children: React.ReactNode
+	description?: string
+}
+function Skeleton(props: Readonly<SkeletonProps>) {
+	return (
+		<Field>
+			<FieldLabel className='flex items-center gap-2' htmlFor={props.id}>
+				{props.icon}
+				{props.label}
+			</FieldLabel>
+			{props.children}
+			{props.description && (
+				<FieldDescription>{props.description}</FieldDescription>
+			)}
 		</Field>
 	)
 }
