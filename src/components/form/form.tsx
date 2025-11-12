0e892f3466a75baaf04af6bbb3875b59a3bc805a
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2025-11-12 01:08:40 +0100

    wrap form in a card

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -15,7 +15,14 @@ import {
 	FieldSet,
 } from '@/components/ui/field'
 import {Button} from '../ui/button'
-import {Camera, ImageIcon, Play, RefreshCw, Scan} from 'lucide-react'
+import {
+	Camera,
+	FolderGit2,
+	ImageIcon,
+	Play,
+	RefreshCw,
+	Scan,
+} from 'lucide-react'
 import {useConfigurationStore} from '@/lib/store'
 import {useReducer, useState} from 'react'
 import {useDebouncedCallback} from 'use-debounce'
@@ -23,6 +30,15 @@ import {CameraSpec} from '@/lib/render/render'
 import {Switch} from '../ui/switch'
 import {Label} from '../ui/label'
 import {Separator} from '../ui/separator'
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '../ui/card'
+import Link from 'next/link'
 
 export default function Form() {
 	const initialCamera = useConfigurationStore.getState().cameraSpec
@@ -83,42 +99,70 @@ export default function Form() {
 
 	return (
 		<form>
-			<Accordion type='single' defaultValue='camera-settings' collapsible>
-				<AccordionItem value='camera-settings'>
-					<AccordionTrigger icon={<Camera size={16} />}>
-						Camera Settings
-					</AccordionTrigger>
-					<AccordionContent>{cameraSettings}</AccordionContent>
-				</AccordionItem>
+			<Card>
+				<CardHeader>
+					<CardTitle className='flex gap-2'>
+						Online Ray Tracer{' '}
+						<Link
+							href='https://github.com/atzhukov/raytracer-web'
+							target='_blank'
+							role='link'
+							aria-label='Link to the GitHub repository'
+						>
+							<FolderGit2 size='1em' />
+						</Link>
+					</CardTitle>
+					<CardDescription>
+						This ray tracer runs locally in your browser. Play around with the
+						camera configuration or scene objects below to render new images:
+					</CardDescription>
+				</CardHeader>
+				<CardContent className='mt-[-16]'>
+					<Accordion type='single' defaultValue='camera-settings' collapsible>
+						<AccordionItem value='camera-settings'>
+							<AccordionTrigger icon={<Camera size={16} />}>
+								Camera Settings
+							</AccordionTrigger>
+							<AccordionContent>{cameraSettings}</AccordionContent>
+						</AccordionItem>
 
-				<AccordionItem value='scene'>
-					<AccordionTrigger icon={<ImageIcon size={16} />}>
-						Scene
-					</AccordionTrigger>
-					<AccordionContent>
-						Yes. It adheres to the WAI-ARIA design pattern.
-					</AccordionContent>
-				</AccordionItem>
-			</Accordion>
-			<Separator className='mb-4' />
-			<div className='flex justify-between'>
-				<div className='flex items-center gap-2'>
-					<Switch
-						id='live'
-						checked={live}
-						onCheckedChange={setLive}
-						aria-label='Start ray tracing immediately'
-						role='switch'
-					/>
-					<Label htmlFor='live' className='flex items-center gap-1.5'>
-						<RefreshCw className='w-[1em]' />
-						Live
-					</Label>
-				</div>
-				<Button variant='outline' size='sm' onClick={(e) => commit(e)}>
-					<Play /> Render
-				</Button>
-			</div>
+						<AccordionItem value='scene'>
+							<AccordionTrigger icon={<ImageIcon size={16} />}>
+								Scene
+							</AccordionTrigger>
+							<AccordionContent>
+								Yes. It adheres to the WAI-ARIA design pattern.
+							</AccordionContent>
+						</AccordionItem>
+					</Accordion>
+					<Separator />
+				</CardContent>
+				<CardFooter>
+					<div className='w-[100%] flex justify-between'>
+						<div className='flex items-center gap-2'>
+							<Switch
+								id='live'
+								checked={live}
+								onCheckedChange={setLive}
+								aria-label='Start ray tracing immediately'
+								role='switch'
+							/>
+							<Label htmlFor='live' className='flex items-center gap-1.5'>
+								<RefreshCw className='w-[1em]' />
+								Live
+							</Label>
+						</div>
+						<Button
+							variant='outline'
+							size='sm'
+							disabled={live}
+							onClick={(e) => commit(e)}
+						>
+							<Play /> Render
+						</Button>
+					</div>
+				</CardFooter>
+			</Card>
 		</form>
 	)
 }
