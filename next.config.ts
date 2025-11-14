3628ef41b884eb6c1b881d868c58e2b89ad30c74
Artem Zhukov <64159790+atzhukov@users.noreply.github.com>
2025-11-14 18:04:10 +0100

    cache .wasm binary

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+	headers: async () => [
+		{
+			source: '/raytracer_bg.wasm',
+			headers: [
+				{
+					key: 'Cache-Control',
+					value: 'public, max-age=216000, immutable',
+				},
+			],
+		},
+	],
+}
 
-export default nextConfig;
+export default nextConfig
